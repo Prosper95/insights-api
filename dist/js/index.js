9ff28ca5bad50d2cde8ce33605844e9249ec07f8
Jordan Eudy <jeudy@redhat.com>
2016-08-02 16:28:11 -0400

    Fixed require('./responseHandlers') since they are not exported anymore

--- dist/js/index.js
+++ dist/js/index.js
@@ -16,12 +16,6 @@ var _urijs2 = _interopRequireDefault(_urijs);
 
 require('babel/browser-polyfill');
 
-var _responseHandlers = require('./responseHandlers');
-
-var handlers = _interopRequireWildcard(_responseHandlers);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /**
